882cd14304262c34ee3912f794e786541c3a164c
Cris Paul Mavida <130685008+Choknath@users.noreply.github.com>
2023-11-12 09:13:54 +0800

    event and profile

---
+++ app/Controllers/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\RestFul\ResourceController;
+use CodeIgniter\API\ResponseTrait;
+use App\Models\UserModel;
+
+
+class AuthController extends ResourceController
+{
+    public function index()
+    {
+        //
+    }
+}
